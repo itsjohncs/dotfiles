159a8d8ca2b741dba83946d86881ca9f31ba68a8
itsjohncs <johnsullivan.pem@gmail.com>
2021-10-19 13:46:01 -0700

    Default to light theme if OS setting unset.
    It's a little unclear to me whether it ever gets set to "Light" now...
    I thought I'd tested both conditions last night though so I'm going to
    assume I did an OK job last night and there are indeed three possible
    states (Light, Dark, unset).

--- src/bash_profile.d/0color-mode.sh
+++ src/bash_profile.d/0color-mode.sh
@@ -1,7 +1,11 @@
 # shellcheck disable=SC2120
 function update_dark_light_mode {
     local MODE
-    MODE="${1-"$(defaults read -g AppleInterfaceStyle)"}"
+    MODE="${1-"$(defaults read -g AppleInterfaceStyle 2>/dev/null)"}"
+    if [[ -z $MODE ]]; then
+        MODE=Light
+    fi
+
     if [[ $MODE == Light || $MODE == Dark ]]; then
         osascript -e "tell application \"Terminal\"
           set current settings of tabs of windows to settings set \"Solarized $MODE\"

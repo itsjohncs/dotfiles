f2277018ec1db890de2e60e2fe08cab12ad18ff9
itsjohncs <johnsullivan.pem@gmail.com>
2021-10-20 02:17:54 -0700

    Add p script.
    This is already useful but does not yet fulfill my vision of a command
    that will prepare my dev setup for me.

---
+++ src/scripts/completions/p.completions.sh
@@ -0,0 +1,10 @@
+function _p_completions {
+    # ls is used over find because it's much faster
+    mapfile -t COMPREPLY < <(
+        compgen \
+            -W "$(ls "$HOME/personal")" \
+            "${COMP_WORDS[1]}"
+    )
+}
+
+complete -F _p_completions p

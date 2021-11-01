3c83ee65b056ea7be8849c3bfcb60994ca4de4f9
itsjohncs <johnsullivan.pem@gmail.com>
2021-11-01 15:27:59 -0700

    Add aliases to help-dotfiles.

--- src/bash_profile.d/0color-mode.sh
+++ src/bash_profile.d/0color-mode.sh
@@ -1,6 +1,6 @@
-## update_dark_light_mode: Changes terminal color profile.
+## color_mode: Changes terminal color profile.
 # shellcheck disable=SC2120
-function update_dark_light_mode {
+function color_mode {
     local MODE
     MODE="${1-"$(defaults read -g AppleInterfaceStyle 2>/dev/null)"}"
     if [[ -z $MODE ]]; then
@@ -11,7 +11,14 @@ function update_dark_light_mode {
         osascript -e "tell application \"Terminal\"
           set current settings of tabs of windows to settings set \"Solarized $MODE\"
         end tell"
+    else
+        echo "USAGE: color_mode [MODE]"
+        echo
+        echo "Updates terminal color profile to match current OS color"
+        echo "Light/Dark setting. MODE may be specified explicitly as 'Light'"
+        echo "or 'Dark'."
+        return 1
     fi
 }
 
-update_dark_light_mode
+color_mode

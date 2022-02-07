fdbab8d320555ac8d686c4c6575459b793a9e00b
itsjohncs <johnsullivan.pem@gmail.com>
2022-02-06 16:31:42 -0800

    Speed up pyenv initialization.

--- src/bash_profile.d/pyenv.sh
+++ src/bash_profile.d/pyenv.sh
@@ -1,1 +1,4 @@
-eval "$(pyenv init -)"
+# I'm unsure of the implications of --no-rehash. Pulled from
+# https://github.com/pyenv/pyenv/issues/784#issuecomment-826444110. If I have
+# weird issues with pyenv in the future, that'll be a good start.
+eval "$(pyenv init --path --no-rehash)"

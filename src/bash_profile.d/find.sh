cf42b7adf3d8d54bfc0f5597ac821177cc01c78e
itsjohncs <johnsullivan.pem@gmail.com>
2022-06-03 18:06:37 -0700

    Allow -regextype to be set. Version check.

--- src/bash_profile.d/find.sh
+++ src/bash_profile.d/find.sh
@@ -1,33 +1,32 @@
 #!/usr/bin/env bash
 
-function find {
-    {
-        local SET_X=0
-        if shopt -po xtrace 1> /dev/null; then
-            SET_X=1
-            set +x
-        fi
-    } 2> /dev/null
-
-    local -a ARGS
-    for i in "$@"; do
-        if [[ $i =~ -i?regex ]]; then
-            ARGS+=(-regextype posix-extended)
-        fi
-
-        ARGS+=("$i")
-    done
-
-    command find "${ARGS[@]}"
-    local RV=$?
-
-    if [[ $SET_X -eq 1 ]]; then
-        set -x
-    fi
-
-    {
-        return $RV
-    } 2> /dev/null
+function __supports_local_dash {
+    local - 2> /dev/null
 }
 
-export -f find
+if __supports_local_dash; then
+    function find {
+        {
+            local -
+            set +xe
+        } 2> /dev/null
+
+        local -a ARGS
+        for i in "$@"; do
+            if [[ $i =~ -i?regex ]]; then
+                ARGS+=(-regextype posix-extended)
+            elif [[ $i = -regextype ]]; then
+                ARGS=("$@")
+                break
+            fi
+
+            ARGS+=("$i")
+        done
+
+        command find "${ARGS[@]}"
+    }
+
+    export -f find
+else
+    echo "WARNING: Version of bash is too old for find shim. Upgrade to >=4.4." 2> /dev/null
+fi

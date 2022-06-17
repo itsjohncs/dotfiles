bcb36357f3fd2f4f2e344fbf2304a44235a703b0
itsjohncs <johnsullivan.pem@gmail.com>
2022-06-17 16:14:11 -0700

    Fix up git-select.
    I broke it when making it cross-platform I think.

--- src/bash_profile.d/shortcuts.sh
+++ src/bash_profile.d/shortcuts.sh
@@ -1,16 +1,22 @@
-alias s="ls"
+function main {
+    alias s="ls"
 
-alias ga="git add"
-alias gc="git checkout"
-alias gb="git branch"
-alias gd="git diff"
-alias gdc="git diff --cached"
-alias gg="git grep --untracked --line-number"
-alias gl="git log"
-alias gm="git commit"
-alias gr="git reset"
-alias gs="git status"
-alias gsh="git show"
-alias mk="cowsay 'Hi! I love you 💞' && clear"
-alias pre="open -a Preview.app"
-alias git="select-git"
+    alias ga="git add"
+    alias gc="git checkout"
+    alias gb="git branch"
+    alias gd="git diff"
+    alias gdc="git diff --cached"
+    alias gg="git grep --untracked --line-number"
+    alias gl="git log"
+    alias gm="git commit"
+    alias gr="git reset"
+    alias gs="git status"
+    alias gsh="git show"
+    alias mk="cowsay 'Hi! I love you 💞' && clear"
+    alias pre="open -a Preview.app"
+
+    local SCRIPT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P )" 
+    alias git="$SCRIPT_DIR/../git-select/src/forward.py"
+}
+
+main

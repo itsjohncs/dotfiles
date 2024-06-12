594b2051b88c1a5a2716f78b1478faec448cc43f
itsjohncs <johnsullivan.pem@gmail.com>
2024-06-12 12:25:42 -0700

    Upgrade default node version.

--- src/bash_profile.d/nvm.sh
+++ src/bash_profile.d/nvm.sh
@@ -7,7 +7,7 @@ export NVM_DIR="$HOME/.nvm"
 
 # Running `nvm use 14.17.6` after sourcing nvm.sh would have the same
 # effect but take an extra second or so to run.
-export PATH="$NVM_DIR/versions/node/v14.17.6/bin:$PATH"
+export PATH="$NVM_DIR/versions/node/v20.14.0/bin:$PATH"
 
 if [[ -s "$NVM_DIR/nvm.sh" ]]; then
     # shellcheck source=/dev/null

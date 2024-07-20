9a7e77739b3257246c9ba55254bad4c7b62aa7a4
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-19 17:15:20 -0700

    Remove pyenv configuration.
    I'm going all-in on Charlie's rye.

--- src/bash_profile.d/pyenv.sh
+++
@@ -1,4 +0,0 @@
-# I'm unsure of the implications of --no-rehash. Pulled from
-# https://github.com/pyenv/pyenv/issues/784#issuecomment-826444110. If I have
-# weird issues with pyenv in the future, that'll be a good start.
-eval "$(pyenv init --path --no-rehash)"

485f6e6194afcb68aefb7ea9b9d092147b37b477
John Sullivan <johnsullivan.pem@gmail.com>
2025-09-04 16:46:43 -0700

    Fix git completion.

--- src/bash_profile.d/zzz-complete-alias.sh
+++ src/bash_profile.d/zzz-complete-alias.sh
@@ -9,9 +9,20 @@ if [[ -n ${BASH_COMPLETION_SCRIPT:-} ]]; then
     # shellcheck source=/dev/null
     source "$SCRIPT_DIR/src/complete-alias/complete_alias"
 
+    # Override the alias body function to provide proper completion for git
+    __compal__get_alias_body() {
+        local cmd="$1"
+        if [[ $cmd == "git" ]]; then
+            echo "git"
+        else
+            local body
+            body="$(alias "$cmd")"
+            echo "${body#*=}" | command xargs
+        fi
+    }
+
     mapfile -t ALIASES < <(
-        printf "%s\n" "${!BASH_ALIASES[@]}" |
-            grep --invert-match "^git$"
+        printf "%s\n" "${!BASH_ALIASES[@]}"
     )
 
     if [[ ${#ALIASES[@]} -gt 0 ]]; then
